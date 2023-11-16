4fb7e83d71020037c6416e497e8b2a1d64edf17a
VanUaTu1 <vkapustjak@gmail.com>
2023-11-16 12:12:17 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,7 @@
 'use strict';
 function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
-    message = "You ordered $[quantity] droids worth $[totalPrice] credits!";    
+    message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;    
     return message;
+    console.log(message);
 }
-
-console.log(makeTransaction(5, 3000));
